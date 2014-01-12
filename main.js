0f1eb00d3eb56fb6c1a521e1448785214e3a7423
Hayato Hiratori <flatbird.hiratori@gmail.com>
2014-01-12 21:18:23 +0900

    Check origin.

---
+++ main.js
@@ -0,0 +1,13 @@
+window.addEventListener('DOMContentLoaded', function() {
+	var elm = document.getElementById('origin');
+	var txt = document.createTextNode(window.location.origin);
+	elm.appendChild(txt);
+	if (window.navigator.mozApps) {
+		var install = document.getElementById('install');
+		install.setAttribute('style', 'display:inline;');
+		install.addEventListener('click', function(evt) {
+			var manifest = window.location.origin + '/manifest.webapp';
+			window.navigator.mozApps(manifest);
+		});
+	}
+});
